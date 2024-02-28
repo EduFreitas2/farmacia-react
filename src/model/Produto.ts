3a1fe2537f05f71e8523ed8c8d5200580f2d8a7c
EduFreitas2 <efnascimento.filho2@gmail.com>
2024-02-28 12:14:12 -0300

    model

---
+++ src/model/Produto.ts
@@ -0,0 +1,11 @@
+import Categoria from './Categoria';
+
+
+export default interface Produto {
+  id: number;
+  descricao: string;
+  foto: string;
+  nome: string;
+  preco: number;
+  tema: Categoria | null;
+}
